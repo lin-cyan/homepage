4e5ddfd3ef34989c5d168ac903407d24a4533448
lancellq <lancellq@gmail.com>
2026-09-25 09:47:15 -0500

    update-1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,15 +11,15 @@ const Footer = () =>{
                     
                     {/* <Image src="/3.png"  width={30} height={30} className={styles.icon} alt="Lama Dev" /> */}
                     {/* <Image src="/4.png"  width={30} height={30} className={styles.icon} alt="Lama Dev" /> */}
-                    <a href="https://github.com/Lanceart">
+                    <a href="https://github.com/lin-cyan">
                     <Image src="/icon_github.jpg"  width={30} height={30} className={styles.icon} alt="Lama Dev" />
                     </a>
 
                     
 
-                    <a href="https://drive.google.com/file/d/18hk47bw9DRWmAcn_VvTlR0ZElI7G6lTY/view?usp=drive_link">
+                    {/* <a href="https://drive.google.com/file/d/18hk47bw9DRWmAcn_VvTlR0ZElI7G6lTY/view?usp=drive_link">
                     <Image src="/icon_resume.png"  width={30} height={30} className={styles.icon} alt="Lama Dev" />
-                    </a>
+                    </a> */}
 
                     {/* <a href="https://www.linkedin.com/in/linqingcyanli">
                     <Image src="/icon_linkedin.png"  width={30} height={30} className={styles.icon} alt="Lama Dev" />
